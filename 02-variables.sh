3938d9a12212f20ccf826e2818ed85d77b0e7cf5
nikhilkothwal <nkothwal@gmail.com>
2025-01-02 23:53:13 +0530

    helloworld text added

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+$PERSON1=$1
+$PERSON2=$2
+
+echo "$PERSON1 :: what are you doing? $PERSON2"
+echo "$PERSON2" :: nothing! wbu? $PERSON1"
+echo "$PERSON1 :: bye then"
+echo "$PERSON2" :: bye!"
