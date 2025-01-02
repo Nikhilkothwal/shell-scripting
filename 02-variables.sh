9dd29b17c4238274bdcedffbd2d66e48f8ad23a2
nikhilkothwal <nkothwal@gmail.com>
2025-01-02 23:57:10 +0530

    helloworld text added

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,6 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1 :: what are you doing? $PERSON2"
-echo "$PERSON2" :: nothing! wbu? $PERSON1"
+echo "$PERSON2 :: nothing! wbu? $PERSON1"
 echo "$PERSON1 :: bye then"
-echo "$PERSON2" :: bye!"
+echo "$PERSON2 :: bye!"
